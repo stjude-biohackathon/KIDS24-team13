85c9de1d4c591b1a77cd8bb70a35433ebe7fe577
Ali Khalighifar <Ali.Khalighifar@stjude.org>
2024-09-04 15:57:49 -0500

    Obtain Z value and return all in integer

--- src/napari_idms/roi_generator_widget.py
+++ src/napari_idms/roi_generator_widget.py
@@ -4,22 +4,7 @@ from qtpy import uic
 from qtpy.QtCore import Qt
 from qtpy.uic import loadUiType
 import sys
-
-# Load the .ui file
-# Ui_CustomWidget, _ = loadUiType("custom_widget.ui")
-
-
-# class CustomWidget(QWidget):
-#     def __init__(self, text, parent=None):
-#         super().__init__(parent)
-
-#         # Create a label to show the text
-#         self.label = QLabel(text)
-        
-#         # Set up the layout and add the label
-#         layout = QVBoxLayout()
-#         layout.addWidget(self.label)
-#         self.setLayout(layout)
+import numpy as np
 
 
 class RoiListWidget(QWidget):
@@ -50,8 +35,6 @@ class ROI_Generator_widget(QWidget):
         abs_file_path = os.path.join(script_dir, '..', 'UI_files', ui_file_name)
         uic.loadUi(abs_file_path, self)
 
-        # Creating the scrollbar for widget list
-        # self.scroll = self.findChild(QListWidget, "roi_lw")
         # Create a QListWidget
         self.list_widget = self.findChild(QListWidget, "roi_lw")
 
@@ -68,10 +51,6 @@ class ROI_Generator_widget(QWidget):
         self.viewer.layers.events.inserted.connect(self.on_layer_added)
 
 
-        # New code to handle shape layer and printing coordinates
-        # self.shapes_layer = self.viewer.add_shapes()  # Create or access a shapes layer in napari
-        # self.shapes_layer.events.data.connect(self.on_shape_drawn)  # Connect event when a new shape is drawn
-
     def on_layer_added(self, event):
         """Check if a new shapes layer has been added."""
         layer = event.value
@@ -86,9 +65,12 @@ class ROI_Generator_widget(QWidget):
         if len(self.shapes_layer.data) > 0:
             # Get the last added shape coordinates
             last_shape = self.shapes_layer.data[-1]
-            print("Coordinates of the drawn shape:", last_shape)
-        else:
-            print("No shape is drawn yet.")
+            last_shape_int = last_shape.astype(int)
+
+            # If there are only two coordinates (x, y), set z to 0 by default
+            if last_shape_int.shape[1] == 2:
+                last_shape_int = np.hstack([last_shape_int, np.zeros((last_shape_int.shape[0], 1), dtype=int)])
+            print("Coordinates of the drawn shape (x, y, z):", last_shape_int)
 
     def create_roi(self, id):
 
