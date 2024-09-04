d4eb060bb770ff63738575646730df833fbf7a0d
Jaison John <jbrother4.jaison@gmail.com>
2024-09-04 15:21:49 -0500

    Scrollable widgets and obtaining coordinates

--- src/napari_idms/roi_generator_widget.py
+++ src/napari_idms/roi_generator_widget.py
@@ -1,8 +1,41 @@
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QComboBox, QLabel, QWidget, QVBoxLayout, QListWidget, QLineEdit
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QComboBox, QLabel, QWidget, QVBoxLayout, QListWidget, QListView, QListWidgetItem, QLabel, QVBoxLayout, QLineEdit
 import os
 from qtpy import uic
 from qtpy.QtCore import Qt
+from qtpy.uic import loadUiType
 import sys
+
+# Load the .ui file
+# Ui_CustomWidget, _ = loadUiType("custom_widget.ui")
+
+
+# class CustomWidget(QWidget):
+#     def __init__(self, text, parent=None):
+#         super().__init__(parent)
+
+#         # Create a label to show the text
+#         self.label = QLabel(text)
+        
+#         # Set up the layout and add the label
+#         layout = QVBoxLayout()
+#         layout.addWidget(self.label)
+#         self.setLayout(layout)
+
+
+class RoiListWidget(QWidget):
+    def __init__(self, text, parent=None):
+        super().__init__(parent)
+
+        # Load the UI file - Main window
+        script_dir = os.path.dirname(__file__)
+        ui_file_name = "roi_list_item.ui"
+        abs_file_path = os.path.join(script_dir, '..', 'UI_files', ui_file_name)
+        uic.loadUi(abs_file_path, self)
+
+        self.roi_lbl = self.findChild(QLabel, "roi_lbl")
+
+        self.roi_lbl.setText(text)
+
 
 class ROI_Generator_widget(QWidget):
     def __init__(self, viewer):
@@ -13,9 +46,18 @@ class ROI_Generator_widget(QWidget):
 
         # Load the UI file - Main window
         script_dir = os.path.dirname(__file__)
-        ui_file_name = "mock_roi_generate_tab.ui"
+        ui_file_name = "roi_generate_tab.ui"
         abs_file_path = os.path.join(script_dir, '..', 'UI_files', ui_file_name)
         uic.loadUi(abs_file_path, self)
+
+        # Creating the scrollbar for widget list
+        # self.scroll = self.findChild(QListWidget, "roi_lw")
+        # Create a QListWidget
+        self.list_widget = self.findChild(QListWidget, "roi_lw")
+
+        # Add some custom widgets to the QListWidget
+        for i in range(5):
+            self.create_roi(i)
 
         # Start from here for the dynamic UI elements
 
@@ -48,7 +90,19 @@ class ROI_Generator_widget(QWidget):
         else:
             print("No shape is drawn yet.")
 
+    def create_roi(self, id):
 
+        # Create the custom widget
+        custom_widget = RoiListWidget(str(id))
+
+        # Wrap the custom widget in a QListWidgetItem
+        list_item = QListWidgetItem()
+        list_item.setSizeHint(custom_widget.sizeHint())
+
+        # Add the widget to the QListWidget
+        self.list_widget.addItem(list_item)
+        self.list_widget.setItemWidget(list_item, custom_widget)
+   
     def register_with_IDMS(self):
         print("Executing this statement ! ")
 
